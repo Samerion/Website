f723a129196ea7b78ad90070c369379b6ae1bb6c
Soaku <dominik@drozak.net>
2021-03-17 08:40:11 +0100

    Add a favicon

--- source/samerion.website/main.d
+++ source/samerion.website/main.d
@@ -9,14 +9,20 @@ import samerion.website.home;
 version (Samerion_Webgen)
 void main() {
 
+    // Prepare the public directory
     mkdirRecurse("public");
 
+    // Create resources
     if (!exists("public/res")) {
 
         symlink("../resources", "public/res");
 
     }
 
+    // Create the favicon
+    copy("resources/favicon-samerion.ico", "public/favicon.ico");
+
+    // Pages
     write("public/index.html", home.render);
     makeBlog();
 
